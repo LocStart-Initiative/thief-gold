98101ad2dcadd53d0ea3920f213d90de758e9092
Matheus Fabris <git@locstart.carameloc.com>
2026-07-14 21:56:39 -0300

    welp, that was completely broken

--- Tooling/STR_to_XLSX.py
+++ Tooling/STR_to_XLSX.py
@@ -23,28 +23,28 @@ for root, subdirs, files in os.walk(folder_from):
             with mmap.mmap(openfile.fileno(), length=0, access=mmap.ACCESS_READ) as mm:
                 mo = re.findall(br'(^\w+):\s*"([^"\\]*(?:\\[\s\S\n]+?[^"\\]*)*\s*)(")', mm, re.MULTILINE)
                 if mo:
-                    for match in mo:
-                        data = {
-                            "id": [],
-                            "string": []
-                        }
+                    data = {
+                        "id": [],
+                        "string": []
+                    }
 
+                    for match in mo:
                         data["id"].append(match[0].decode("cp1252"))
                         data["string"].append(match[1].decode("cp1252"))
 
                         # print(data)
 
-                        # save to excel file
-                        df = pd.DataFrame(data)
+                    # save to excel file
+                    df = pd.DataFrame(data)
 
-                        relative = root.replace(folder_from, "")[1:]
-                        absolute = os.path.join(folder_to, relative)
+                    relative = root.replace(folder_from, "")[1:]
+                    absolute = os.path.join(folder_to, relative)
 
-                        if not os.path.isdir(absolute):
-                            Path(absolute).mkdir(parents=True)
+                    if not os.path.isdir(absolute):
+                        Path(absolute).mkdir(parents=True)
 
-                        target_file = os.path.join(absolute, os.path.splitext(file)[0] + ".xlsx")
+                    target_file = os.path.join(absolute, os.path.splitext(file)[0] + ".xlsx")
 
-                        df.to_excel(target_file, index=False)
+                    df.to_excel(target_file, index=False)
 
-                        openfile.close()
+                    openfile.close()
